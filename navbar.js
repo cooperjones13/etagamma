0b7440ed16f23ad801277176a8bf8ae55771ffa9
Cooper Jones <cooper.jones@colorado.edu>
2019-10-24 01:04:07 -0600

    Responsiveness

---
+++ navbar.js
@@ -0,0 +1,11 @@
+
+
+// setTimeout(function(){
+//   document.getElementById("name").setAttribute("style", "transition: color 1s;");
+// }, 300);
+
+
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+if(width <= 600){
+
+}
