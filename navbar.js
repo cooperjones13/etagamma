fb64315f90f07c619dfff3c2da59f5422ce14cfe
Cooper Jones <cooper.jones@colorado.edu>
2019-10-24 04:04:14 -0600

    Responsive Navigation

--- navbar.js
+++ navbar.js
@@ -3,9 +3,31 @@
 // setTimeout(function(){
 //   document.getElementById("name").setAttribute("style", "transition: color 1s;");
 // }, 300);
+window.addEventListener("resize", resize);
 
+document.addEventListener("DOMContentLoaded",function(){
+  setTimeout(function(){
+    document.getElementById("name").setAttribute("style", "transition: color 1s;");
+  }, 300);
 
-var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-if(width <= 600){
+  resize();
+});
 
+function resize(){
+  var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+  if(width <= 600){
+    document.getElementById("name").innerHTML = "Theta Tau";
+  } else {
+    document.getElementById("name").innerHTML = "Theta Tau - CU Boulder";
+    document.getElementById("navBtns").className = "navigation";
+  }
+}
+
+function onMenuClick(){
+  var el = document.getElementById("navBtns");
+  if(el.className === "navigation"){
+    el.className += "-responsive";
+  } else {
+    el.className = "navigation";
+  }
 }
